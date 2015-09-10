b938c34b960ebbcd3d4ff3060d817a091389778a
Karl Voelker <karl@karlv.net>
2015-09-09 21:12:13 -0700

    Prepare for release

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ Robin is a build daemon for Haskell development.
     robin (start | stop | build | watch | debug)
 
 This daemon rebuilds a cabalized Haskell project whenever a source file
-changes.
+changes. It runs one of the following commands:
+
+* `./robin.sh`, if that file exists, or
+* `stack build`, if `stack.yaml` exists, or
+* `cabal build`, otherwise.
 
 ## Commands
 
@@ -20,10 +24,8 @@ changes.
 
 The default command is _build_.
 
-Note: the _current project_ is identified by the lone cabal file in the root
-directory of the git working copy of the current directory. (In other words,
-`$(git rev-parse --show-toplevel)/*.cabal`.) I'm not sure if this is the ideal
-definition.
+Note: the _current project_ is assumed to be the root of the git working copy
+which contains the current directory.  definition.
 
 
 ## Vim integration
--- src/Util.hs
+++ src/Util.hs
@@ -1,14 +1,7 @@
 
-module Util where
+module Util (todo, leftMaybe, readMaybe, getRepoRoot) where
 
 import Data.Char
-import Data.List
-import Data.Maybe
-import Distribution.PackageDescription
-import Distribution.PackageDescription.Configuration
-import Distribution.PackageDescription.Parse
-import Distribution.Verbosity
-import System.Directory
 import System.Process
 
 todo :: a
@@ -22,24 +15,9 @@ readMaybe xs = case reads xs of
   [(x, "")] -> Just x
   _ -> Nothing
 
-buildInfos :: PackageDescription -> [BuildInfo]
-buildInfos pkg = libs ++ execs
-  where
-    libs = maybeToList . fmap libBuildInfo . library $ pkg
-    execs = map buildInfo . executables $ pkg
-
 trim :: String -> String
 trim = let f = reverse . dropWhile isSpace in f . f
 
 getRepoRoot :: IO String
 getRepoRoot = readProcess "git" ["rev-parse", "--show-toplevel"] "" >>= return . trim
-
-getPkgDesc :: String -> IO (Maybe PackageDescription)
-getPkgDesc dir = do
-  files <- getDirectoryContents dir
-  case filter (".cabal" `isSuffixOf`) $ files of
-    [cabalFile] ->
-      Just . flattenPackageDescription
-      <$> readPackageDescription silent (dir ++ "/" ++ cabalFile)
-    _ -> return Nothing
 
